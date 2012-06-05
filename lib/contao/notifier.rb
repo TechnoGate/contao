a725443f18f623259ff233d4715421177bae734b
Wael M. Nasreddine <wael.nasreddine@gmail.com>
2012-06-05 19:45:05 +0200

    Add a warn method to the notifier

--- lib/contao/notifier.rb
+++ lib/contao/notifier.rb
@@ -19,6 +19,20 @@ module TechnoGate
       def self.notify(*args, &block)
         instance.notify(*args, &block)
       end
+
+      def warn(message, options = {})
+        if ::Guard::UI.send(:color_enabled?)
+          message = "\e[0;34mContao>>\e[0m \e[0;33m#{message}\e[0m"
+        else
+          message = "Contao>> #{message}"
+        end
+
+        ::Guard::UI.info(message, options)
+      end
+
+      def self.warn(*args, &block)
+        instance.warn(*args, &block)
+      end
     end
   end
 end
--- spec/lib/contao/notifier_spec.rb
+++ spec/lib/contao/notifier_spec.rb
@@ -45,6 +45,44 @@ module TechnoGate
           klass.notify(@message, @options)
         end
       end
+
+      describe '#warn' do
+        before :each do
+          @message = "Hello"
+          @output  = "Contao>> #{@message}"
+          @colored_output = "\e[0;34mContao>>\e[0m \e[0;33m#{@message}\e[0m"
+          @options = {title: "Hello, World!"}
+
+          ::Guard::UI.stub(:color_enabled?).and_return(false)
+        end
+
+        it {should respond_to :warn}
+
+        it "should call guard ui" do
+          ::Guard::UI.should_receive(:info).with(@output, {})
+
+          subject.warn(@message)
+        end
+
+        it "should send whatever options passed to the info method" do
+          ::Guard::UI.should_receive(:info).with(@output, @options)
+
+          subject.warn(@message, @options)
+        end
+
+        it "should use colors if enabled" do
+          ::Guard::UI.should_receive(:color_enabled?).once.and_return(true)
+          ::Guard::UI.should_receive(:info).with(@colored_output, @options)
+
+          subject.warn(@message, @options)
+        end
+
+        it "should be accessible at class level" do
+          klass.any_instance.should_receive(:warn).with(@message, @options)
+
+          klass.warn(@message, @options)
+        end
+      end
     end
   end
 end
