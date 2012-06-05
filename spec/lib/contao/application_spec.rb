5b43a068a3c841199e022f3d820819a2b29fe401
Wael M. Nasreddine <wael.nasreddine@gmail.com>
2012-06-05 17:10:03 +0200

    Move some configs to a shared example

--- spec/lib/contao/application_spec.rb
+++ spec/lib/contao/application_spec.rb
@@ -6,85 +6,59 @@ module TechnoGate
       subject { Contao::Application.instance }
       let(:klass) { Contao::Application }
 
-      it_should_behave_like "Singleton"
+      it_should_behave_like 'Config'
 
-      it "should be an open struct" do
-        subject.class.superclass.should == OpenStruct
+      describe '#linkify', :fakefs do
+        before :each do
+          stub_filesystem!
+        end
+
+        it "should call #exhaustive_list_of_files_to_link" do
+          subject.should_receive(:exhaustive_list_of_files_to_link).with(
+            Contao.expandify(Application.config.contao_path),
+            Contao.expandify(Application.config.contao_public_path)
+          ).once.and_return []
+
+          subject.linkify
+        end
+
+        it "should actually link the files" do
+          subject.linkify
+
+          File.exists?('/root/public/non_existing_folder').should be_true
+          File.exists?('/root/public/system/modules/some_extension').should be_true
+        end
+
+        it "should be accessible at class level" do
+          subject.should_receive(:linkify).once
+
+          Application.linkify
+        end
       end
 
-      it "should have a config as a superclass" do
-        subject.config.class.should == OpenStruct
+      describe '#exhaustive_list_of_files_to_link', :fakefs do
+        before :each do
+          stub_filesystem!
+        end
+
+        it "should return the correct list of files" do
+          list = subject.send :exhaustive_list_of_files_to_link, '/root/contao', '/root/public'
+          list.should == [
+            ['/root/contao/non_existing_folder', '/root/public/non_existing_folder'],
+            ['/root/contao/system/modules/some_extension', '/root/public/system/modules/some_extension']
+          ]
+        end
       end
 
-      describe "config" do
-        it "should set a configuration variable using a block" do
-          Contao::Application.configure do
-            config.foo = :bar
-          end
+      describe '#name' do
+        it {should respond_to :name}
 
-          Contao::Application.instance.config.foo.should == :bar
+        it "should return the correct name" do
+          subject.name.should == 'root'
         end
 
-        it "should be accessible form the class level" do
-          Contao::Application.configure do
-            config.foo = :bar
-          end
-
-          Contao::Application.config.foo.should == :bar
-        end
-
-        describe '#linkify', :fakefs do
-          before :each do
-            stub_filesystem!
-          end
-
-          it "should call #exhaustive_list_of_files_to_link" do
-            subject.should_receive(:exhaustive_list_of_files_to_link).with(
-              Contao.expandify(Application.config.contao_path),
-              Contao.expandify(Application.config.contao_public_path)
-            ).once.and_return []
-
-            subject.linkify
-          end
-
-          it "should actually link the files" do
-            subject.linkify
-
-            File.exists?('/root/public/non_existing_folder').should be_true
-            File.exists?('/root/public/system/modules/some_extension').should be_true
-          end
-
-          it "should be accessible at class level" do
-            subject.should_receive(:linkify).once
-
-            Application.linkify
-          end
-        end
-
-        describe '#exhaustive_list_of_files_to_link', :fakefs do
-          before :each do
-            stub_filesystem!
-          end
-
-          it "should return the correct list of files" do
-            list = subject.send :exhaustive_list_of_files_to_link, '/root/contao', '/root/public'
-            list.should == [
-              ['/root/contao/non_existing_folder', '/root/public/non_existing_folder'],
-              ['/root/contao/system/modules/some_extension', '/root/public/system/modules/some_extension']
-            ]
-          end
-        end
-
-        describe '#name' do
-          it {should respond_to :name}
-
-          it "should return the correct name" do
-            subject.name.should == 'root'
-          end
-
-          it "should be accessible at class level" do
-            Application.name.should == 'root'
-          end
+        it "should be accessible at class level" do
+          Application.name.should == 'root'
         end
       end
     end
